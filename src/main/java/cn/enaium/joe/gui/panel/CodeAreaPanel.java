a8e0bbaee37f9321bdacd290eb51ed177cea0601
Enaium <wyy-666@hotmail.com>
2022-07-19 09:36:33 +0800

    textarea find

---
+++ src/main/java/cn/enaium/joe/gui/panel/CodeAreaPanel.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2022 Enaium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.enaium.joe.gui.panel;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.Theme;
+import org.fife.ui.rtextarea.RTextScrollPane;
+import org.fife.ui.rtextarea.SearchContext;
+import org.fife.ui.rtextarea.SearchEngine;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+/**
+ * @author Enaium
+ */
+public class CodeAreaPanel extends JPanel implements ActionListener {
+
+    private RSyntaxTextArea textArea;
+    private JTextField searchField;
+    private JCheckBox regexCB;
+    private JCheckBox matchCaseCB;
+
+    public CodeAreaPanel() {
+        super(new BorderLayout());
+        textArea = new RSyntaxTextArea();
+        textArea.setCodeFoldingEnabled(true);
+        textArea.setEditable(false);
+        Theme theme = null;
+        try {
+            theme = Theme.load(getClass().getResourceAsStream("/org/fife/ui/rsyntaxtextarea/themes/dark.xml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        theme.apply(textArea);
+
+        add(new RTextScrollPane(textArea), BorderLayout.CENTER);
+        JToolBar toolBar = new JToolBar();
+        searchField = new JTextField(30);
+        toolBar.add(searchField);
+        final JButton nextButton = new JButton("Find Next");
+        nextButton.setActionCommand("FindNext");
+        nextButton.addActionListener(this);
+        toolBar.add(nextButton);
+        searchField.addActionListener(e -> nextButton.doClick(0));
+        JButton prevButton = new JButton("Find Previous");
+        prevButton.setActionCommand("FindPrev");
+        prevButton.addActionListener(this);
+        toolBar.add(prevButton);
+        regexCB = new JCheckBox("Regex");
+        toolBar.add(regexCB);
+        matchCaseCB = new JCheckBox("Match Case");
+        toolBar.add(matchCaseCB);
+        add(toolBar, BorderLayout.NORTH);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        // "FindNext" => search forward, "FindPrev" => search backward
+        String command = e.getActionCommand();
+        boolean forward = "FindNext".equals(command);
+
+        // Create an object defining our search parameters.
+        SearchContext context = new SearchContext();
+        String text = searchField.getText();
+        if (text.length() == 0) {
+            return;
+        }
+        context.setSearchFor(text);
+        context.setMatchCase(matchCaseCB.isSelected());
+        context.setRegularExpression(regexCB.isSelected());
+        context.setSearchForward(forward);
+        context.setWholeWord(false);
+
+        SearchEngine.find(textArea, context);
+    }
+
+    public RSyntaxTextArea getTextArea() {
+        return textArea;
+    }
+}
--- src/main/java/cn/enaium/joe/gui/panel/file/tabbed/tab/DecompileTabPanel.java
+++ src/main/java/cn/enaium/joe/gui/panel/file/tabbed/tab/DecompileTabPanel.java
@@ -1,19 +1,12 @@
 package cn.enaium.joe.gui.panel.file.tabbed.tab;
 
-import cn.enaium.joe.gui.panel.CodeArea;
+import cn.enaium.joe.gui.panel.CodeAreaPanel;
 import cn.enaium.joe.util.ASyncUtil;
 import cn.enaium.joe.util.CfrUtil;
-import org.benf.cfr.reader.PluginRunner;
-import org.benf.cfr.reader.api.ClassFileSource;
-import org.benf.cfr.reader.bytecode.analysis.parse.utils.Pair;
 import org.fife.ui.rtextarea.RTextScrollPane;
-import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.ClassNode;
 
 import java.awt.*;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
 
 /**
  * @author Enaium
@@ -22,12 +15,12 @@ public class DecompileTabPanel extends ClassNodeTabPanel {
     public DecompileTabPanel(ClassNode classNode) {
         super(classNode);
         setLayout(new BorderLayout());
-        CodeArea codeArea = new CodeArea();
-        codeArea.setSyntaxEditingStyle("text/java");
+        CodeAreaPanel codeAreaPanel = new CodeAreaPanel();
+        codeAreaPanel.getTextArea().setSyntaxEditingStyle("text/java");
         ASyncUtil.execute(() -> {
-            codeArea.setText(CfrUtil.getSource(classNode));
+            codeAreaPanel.getTextArea().setText(CfrUtil.getSource(classNode));
         });
-        codeArea.setCaretPosition(0);
-        add(new RTextScrollPane(codeArea));
+        codeAreaPanel.getTextArea().setCaretPosition(0);
+        add(codeAreaPanel);
     }
 }
