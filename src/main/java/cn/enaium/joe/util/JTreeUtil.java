e72eba74bc228b06ac8ab329007ef32b7d31d021
Enaium <wyy-666@hotmail.com>
2022-07-19 10:18:53 +0800

    search

---
+++ src/main/java/cn/enaium/joe/util/JTreeUtil.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 Enaium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.enaium.joe.util;
+
+import cn.enaium.joe.gui.panel.file.tree.node.ClassTreeNode;
+import cn.enaium.joe.gui.panel.file.tree.node.MethodTreeNode;
+import cn.enaium.joe.gui.panel.file.tree.node.PackageTreeNode;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
+public class JTreeUtil {
+    public static void setTreeExpandedState(JTree tree, boolean expanded) {
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getModel().getRoot();
+        setNodeExpandedState(tree, node, expanded);
+    }
+
+    private static void setNodeExpandedState(JTree tree, DefaultMutableTreeNode node, boolean expanded) {
+        Enumeration<TreeNode> children = node.children();
+        while (children.hasMoreElements()) {
+            setNodeExpandedState(tree, (DefaultMutableTreeNode) children.nextElement(), expanded);
+        }
+
+        if (!expanded && node.isRoot()) {
+            return;
+        }
+
+        if (node instanceof PackageTreeNode) {
+            PackageTreeNode packageTreeNode = (PackageTreeNode) node;
+            if (packageTreeNode instanceof ClassTreeNode) {
+                return;
+            }
+        }
+
+        TreePath path = new TreePath(node.getPath());
+        if (expanded) {
+            tree.expandPath(path);
+        } else {
+            tree.collapsePath(path);
+        }
+    }
+}
