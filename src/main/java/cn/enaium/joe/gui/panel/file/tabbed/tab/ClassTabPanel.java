11eae0b39108985baef887bcd0fb424fac14c337
Enaium <wyy-666@hotmail.com>
2022-07-30 18:25:51 +0800

    rename

--- src/main/java/cn/enaium/joe/gui/panel/file/tabbed/tab/ClassTabPanel.java
+++ src/main/java/cn/enaium/joe/gui/panel/file/tabbed/tab/ClassTabPanel.java
@@ -8,14 +8,15 @@ import java.awt.*;
 /**
  * @author Enaium
  */
-public class FileTabPanel extends JPanel {
-    public FileTabPanel(ClassNode classNode) {
+public class ClassTabPanel extends JPanel {
+    public ClassTabPanel(ClassNode classNode) {
         super(new BorderLayout());
         JTabbedPane jTabbedPane = new JTabbedPane();
         jTabbedPane.setTabPlacement(JTabbedPane.BOTTOM);
         jTabbedPane.addTab("BytecodeView", new TraceBytecodeTabPanel(classNode));
         jTabbedPane.addTab("DecompileView", new DecompileTabPanel(classNode));
         jTabbedPane.addTab("VisitorEdit", new ASMifierTablePanel(classNode));
+        jTabbedPane.addTab("InfoEdit", new ClassInfoTabPanel(classNode));
         add(jTabbedPane);
     }
 }
