da54795de289b63b283c8f3fd7d3f12d3b07b464
Enaium <wyy-666@hotmail.com>
2022-08-22 11:19:17 +0800

    catch the last exception when accept by the Class Reader

--- src/main/java/cn/enaium/joe/task/InputJarTask.java
+++ src/main/java/cn/enaium/joe/task/InputJarTask.java
@@ -19,6 +19,8 @@ package cn.enaium.joe.task;
 import cn.enaium.joe.JavaOctetEditor;
 import cn.enaium.joe.config.extend.ApplicationConfig;
 import cn.enaium.joe.jar.Jar;
+import cn.enaium.joe.util.IOUtil;
+import cn.enaium.joe.util.MessageUtil;
 import cn.enaium.joe.util.Util;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.ClassNode;
@@ -58,23 +60,20 @@ public class InputJarTask extends AbstractTask<Jar> {
             while (entries.hasMoreElements()) {
                 JarEntry jarEntry = entries.nextElement();
                 if (jarEntry.getName().endsWith(".class")) {
-                    ClassReader classReader = new ClassReader(jarFile.getInputStream(new JarEntry(jarEntry.getName())));
+                    ClassReader classReader = new ClassReader(IOUtil.getBytes(jarFile.getInputStream(new JarEntry(jarEntry.getName()))));
                     ClassNode classNode = new ClassNode();
                     try {
                         classReader.accept(classNode, ClassReader.EXPAND_FRAMES);
                     } catch (Throwable throwable) {
-                        classReader.accept(classNode, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
+                        try {
+                            classReader.accept(classNode, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
+                        } catch (Throwable last) {
+                            Logger.error("FILE:{},{}", jarEntry.getName(), last);
+                        }
                     }
                     jar.classes.put(jarEntry.getName(), classNode);
                 } else if (!jarEntry.isDirectory()) {
-                    ByteArrayOutputStream output = new ByteArrayOutputStream();
-                    byte[] buffer = new byte[1024 * 4];
-                    int n;
-                    InputStream inputStream = jarFile.getInputStream(new JarEntry(jarEntry.getName()));
-                    while (-1 != (n = inputStream.read(buffer))) {
-                        output.write(buffer, 0, n);
-                    }
-                    jar.resources.put(jarEntry.getName(), output.toByteArray());
+                    jar.resources.put(jarEntry.getName(), IOUtil.getBytes(jarFile.getInputStream(new JarEntry(jarEntry.getName()))));
                 }
                 setProgress((int) ((loaded++ / files) * 100f));
             }
